338d3d799adac8e068649b93105b43df09d5e8d4
heejin99 <wkdgmlwls30@naver.com>
2021-03-09 23:34:10 +0900

    파이썬 GUI
    2. 버튼 생성

---
+++ gui_basic/2_button.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+root = Tk()
+root.title("Heejin GUI")
+
+btn1 = Button(root, text="버튼1")
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=10, text="버튼2") # padx, pady는 여백
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5, text="버튼3")
+btn3.pack()
+
+btn4 = Button(root, width=10, height=3, text="버튼4") # 버튼의 가로 세로 높이는 고정된 크기
+btn4.pack()
+
+btn5 = Button(root, fg="red", bg="yellow", text="버튼5")
+btn5.pack()
+
+photo = PhotoImage(file="gui_basic/img.png")
+btn6 = Button(root, image=photo)
+btn6.pack()
+
+def btncmd():
+    print("버튼이 클릭되었어요.")
+    
+btn7 = Button(root, text="동작하는 버튼", command = btncmd)
+btn7.pack()
+
+root.mainloop()
